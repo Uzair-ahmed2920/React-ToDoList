14fa4d93705dca7a6e9bf72e21d40951f8c5db6d
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2022-07-29 03:55:31 +0500

    Style Done

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,10 +1,15 @@
+import { MdAddCircle } from "react-icons/md";
 
 
 function App() {
   return (
-    <div className="mainDiv">Hello
+    <div className="mainDiv">
     <div className="centerDiv">
-      from center 
+      <br/>
+      <h1>ToDo List</h1>  
+      <br/>
+      <input type="text" placeholder="Add an Items"></input>
+      <MdAddCircle size={'4rem'} style={{color: 'rgb(9, 61, 38)', margin: '0px 0px -20px 11px' }}/>
     </div>
     </div>
   );
